4a8f3ad523a79bcd4665d0f69c3d0a650af6649d
kevin Wairi <kevinwairi@gmail.com>
2023-03-30 20:55:51 +0300

    completed assignment

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
+
+
+const likers = document.getElementsByClassName('like-glyph')
+const modal = document.getElementById('modal')
+
+
+// event handler calls a function that will "mock" the behavior of a backend server. 
+let count = 0;
+const liked = Array.from(likers)
+liked.forEach(liker=>{
+  liker.addEventListener('click',function (){
+ 
+
+    // server connections using mimicServerCall to response to a user action,
+   
+      mimicServerCall()
+      .then((isRandomFailure)=>{
+        count++
+        console.log(isRandomFailure)
+        if (!(count%2==0)) {
+          liker.textContent = FULL_HEART,
+          liker.classList.add('activated-heart') 
+        } else{
+          liker.textContent = EMPTY_HEART,
+          liker.classList.remove('activated-heart');
+        }}
+        )
+        
+     
+        // the catch method after .then
+      .catch((isRandomFailure) => {
+        modal.classList.remove('hidden')
+        modal.textContent = isRandomFailure;
+        console.log(isRandomFailure);
+        setTimeout(()=>{
+          modal.classList.add('hidden')
+        },3000)
+      }
+      )
+      })      
+  }
+)
 
 
 
